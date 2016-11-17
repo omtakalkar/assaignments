f720395f67adb4bcb306f8651d03304672c0aea9
omtakalkar <om.takalkar@gmail.com>
2016-11-17 17:25:11 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,16 +1,19 @@
-
 import java.util.Scanner;
 
-public class factOfNumbers {
+public class factOfNumbers 
+{
 
-	public static void main(String args[]){
+	public static void main(String args[])
+	{
 		int i,fact=1;
 		 System.out.println("Enter a number: \n");
 	      Scanner in = new Scanner(System.in);
 	    int  n = in.nextInt();
-		for(i=1;i<=n;i++){
-		fact=fact*i;
+		for(i=1;i<=n;i++)
+		{
+			fact=fact*i;
 		}
 		System.out.println(fact);
 	
-}}
+	}
+}
