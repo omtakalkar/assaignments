24e955c931ae06eac9ea7082908d82d41103f48b
omtakalkar <om.takalkar@gmail.com>
2016-11-22 17:43:32 +0530

    Create tryCatch.java

---
+++ tryCatch.java
@@ -0,0 +1,22 @@
+public class tryCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number1,number2;
+		
+		try
+		{
+			number1 =46;
+			number2=598365/number1;
+			
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("error");
+		}
+		
+
+	}
+
+}
